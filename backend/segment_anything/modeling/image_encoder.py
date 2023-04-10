a65becb68f908a1a9d772dde6d99404724628f96
Keigo-Iwakuma <sym90chn@gmail.com>
2023-04-11 00:36:29 +0900

    update backend/modeling/image_encoder.py

--- backend/segment_anything/modeling/image_encoder.py
+++ backend/segment_anything/modeling/image_encoder.py
@@ -54,6 +54,42 @@ class ImageEncoderVit(nn.Module):
         self.patch_embed = PatchEmbed
 
 
+def get_rel_pos(
+    q_size: int,
+    k_size: int,
+    rel_pos: torch.Tensor,
+) -> torch.Tensor:
+    """
+    Get relative position embedding according to the relative positions of query and key size.
+
+    Args:
+        q_size (int): query size
+        k_size (int): key size
+        rel_pos (torch.Tensor): relative position embedding (L, C)
+
+    Returns:
+        torch.Tensor: relative position embedding
+    """
+    max_rel_dist = int(2 * max(q_size, k_size) - 1)
+    # Interpolate rel pos if needed.
+    if rel_pos.shape[0] != max_rel_dist:
+        rel_pos_resized = F.interpolate(
+            rel_pos.reshape(1, rel_pos.shape[0], 1).permute(0, 2, 1),
+            size=max_rel_dist,
+            mode="linear",
+        )
+        rel_pos_resized = rel_pos_resized.reshape(-1, max_rel_dist).permute(1, 0)
+    else:
+        rel_pos_resized = rel_pos
+    
+    # Scale the coords with short length if shapes for q and k are different.
+    q_coords = torch.arange(q_size)[:, None] * max(k_size / q_size, 1.)
+    k_coords = torch.arange(k_size)[:, None] * max(q_size / k_size, 1.)
+    relative_coords = (q_coords + k_coords) + (k_size - 1) * max(q_size / k_size, 1.)
+
+    return rel_pos_resized[relative_coords.long()]
+
+
 class PatchEmbed(nn.Module):
     """
     Image to Patch Embedding.
