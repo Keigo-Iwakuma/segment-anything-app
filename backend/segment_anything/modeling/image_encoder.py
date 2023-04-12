bd45d17fdae4be737839625b991851935a99e511
Keigo-Iwakuma <sym90chn@gmail.com>
2023-04-12 23:49:37 +0900

    add Attention class into image_encoder.py

--- backend/segment_anything/modeling/image_encoder.py
+++ backend/segment_anything/modeling/image_encoder.py
@@ -53,7 +53,62 @@ class ImageEncoderVit(nn.Module):
 
         self.patch_embed = PatchEmbed
 
-    # qkv with shape (3, B, nHead, H * W, C)
+
+class Attention(nn.Module):
+    """Multi-head attention with relative position encoding."""    
+
+    def __init__(
+        self,
+        dim: int,
+        num_heads: int = 8,
+        qkv_bias: bool = True,
+        use_rel_pos: bool = False,
+        rel_pos_zero_init: bool = True,
+        input_size: Optional[Tuple[int, int]] = None,
+    ) -> None:
+        """
+        Args:
+            dim (int): number of input channels
+            num_heads (int): number of attention heads
+            qkv_bias (bool): enable bias for qkv if True
+            use_rel_pos (bool): use relative position embedding
+            rel_pos_zero_init (bool): use zero init for relative position bias
+            input_size (tuple[int]): height and width of input
+        """
+        super().__init__()
+        self.num_heads = num_heads
+        head_dim = dim // num_heads
+        self.scale = head_dim ** -0.5
+
+        self.qkv = nn.Linear(dim, dim * 3, bias=qkv_bias)
+        self.proj = nn.Linear(dim, dim)
+
+        self.use_rel_pos = use_rel_pos
+        if self.use_rel_pos:
+            assert (
+                input_size is not None
+            ), "input_size must be specified when use_rel_pos is True"
+            # initialize relative position embeddings
+            self.rel_pos_h = nn.Parameter(torch.zeros((2 * input_size[0] - 1, head_dim)))
+            self.rel_pos_w = nn.Parameter(torch.zeros((2 * input_size[1] - 1, head_dim)))
+        
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        B, H, W, _ = x.shape
+        # qkv with shape (3, B, nHead, H * W, C)
+        qkv = self.qkv(x).reshape(B, H * W, 3, self.num_heads, -1).permute(2, 0, 3, 1, 4)
+        # q, k, v with shape (B, nHead, H * W, C)
+        q, k, v = qkv.reshape(3, B * self.num_heads, H * W, -1).unbind(0)
+
+        attn = (q @ k.transpose(-2, -1)) * self.scale
+
+        if self.use_rel_pos:
+            attn = add_decomposed_rel_pos(attn, q, self.rel_pos_h, self.rel_pos_w, (H, W), (H, W)) 
+        
+        attn = attn.softmax(dim=-1)
+        x = (attn @ v).view(B, self.num_heads, H, W, -1).permute(0, 2, 3, 1, 4).reshape(B, H, W, -1)
+        x = self.proj(x)
+
+        return x
 
 
 def window_partition(x: torch.Tensor, window_size: int) -> Tuple[torch.Tensor, Tuple[int, int]]:
